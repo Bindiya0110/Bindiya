2b01ab186ed23e23a4a92f5b129a63ef298fab0a
Bindiya0110 <139944657+Bindiya0110@users.noreply.github.com>
2023-08-24 11:31:04 +0530

    24/8/2023

---
+++ assinment/module2/conditional.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int k;
+	printf("Enter a number:"); //input the value of 'k' from user
+	scanf("%d",&k);
+	if(k==5)
+	{
+		printf("Value of the k is: 5.");
+	}
+	return 0;
+}
